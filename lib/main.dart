78fdc6605447ed708717403b9b43fdd8472b32a0
Otniel113 <otniel.abiezer.saragih@gmail.com>
2022-05-25 08:41:10 +0700

    Rollback

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Rent A Car';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePageWidget(),
+    );
+  }
+}
+
+class MyHomePageWidget extends StatefulWidget {
+  const MyHomePageWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePageWidget> createState() => _MyHomePageWidgetState();
+}
+
+class _MyHomePageWidgetState extends State<MyHomePageWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static final List<Widget> _widgetOptions = <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          child: Text('Pilih jenis Kendaraan'),
+        ),
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.redAccent)
+          ),
+          child: const Center(child: Text('Mobil Kecil')),
+        ),
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.redAccent)
+          ),
+          child: const Center(child: Text('Mobil Besar')),
+        ),
+        Container(
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(20.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.redAccent)
+          ),
+          child: const Center(child: Text('Minibus')),
+        ),
+      ],
+    ),
+    const Text(
+      'Halaman Booking',
+      style: optionStyle,
+    ),
+    // const Align(
+    //   alignment: Alignment.centerLeft,
+    //   child: TabBar(
+    //     tabs: [
+    //       Tab(text: 'History'),
+    //       Tab(text: 'Ongoing'),
+    //     ],
+    //   ),
+    // ),
+    const Text(
+      'Halaman Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rent A Car'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.checklist),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_pin),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red[500],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}

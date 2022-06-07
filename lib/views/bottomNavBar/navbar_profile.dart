8ce9100402194fa29dbcc6427b56b9da99254a9e
winicofazry <nikofazry@gmail.com>
2022-06-07 22:18:19 +0700

    profile detail

--- lib/views/bottomNavBar/navbar_profile.dart
+++ lib/views/bottomNavBar/navbar_profile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'profile/profile_detail.dart';
 class NavbarProfile extends StatefulWidget {
   const NavbarProfile({Key? key}) : super(key: key);
 
@@ -8,10 +8,57 @@ class NavbarProfile extends StatefulWidget {
 }
 
 class _NavbarProfileState extends State<NavbarProfile> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Halaman Profile',
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              enabled: false,
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'James',
+              ),
+              style:TextStyle(fontWeight: FontWeight.bold)
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              enabled: false,
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'James@gmail.com',
+              ),
+              style:TextStyle(fontWeight: FontWeight.bold)
+            ),
+          ),
+          Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: ElevatedButton(
+                child: const Text('Edit'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const ProfilePage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red[900],
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
---
+++ lib/views/bottomNavBar/profile/profile_detail.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.red[900], title: const Text('Edit Profile')),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username'
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red[900],
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}

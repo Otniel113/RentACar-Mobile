729d7d95a0f8785551cde7354a5a8957a3a9aaa3
auliaaliefrr <101696378+auliaaliefrr@users.noreply.github.com>
2022-06-25 22:19:50 +0700

    Update navbar_booking.dart

--- lib/views/bottomNavBar/navbar_booking.dart
+++ lib/views/bottomNavBar/navbar_booking.dart
@@ -7,11 +7,60 @@ class NavbarBooking extends StatefulWidget {
   State<NavbarBooking> createState() => _NavbarBookingState();
 }
 
+
 class _NavbarBookingState extends State<NavbarBooking> {
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Halaman Booking',
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: 1,
+      itemBuilder: (BuildContext context, int index) {
+        return new Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.car_rental_rounded),
+              title: Text('Toyota Avanza'),
+              subtitle: Text('Tangkuban Perahu\nD 1111 AAA'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                 TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Toyota Avanza'),
+          content: const Text(''+
+          'Plat Mobil : D 1111 AAA\n'+
+          'Tujuan : Tangkuban Perahu\n'+
+          'Driver : Garok\n'+
+          'Tgl Pinjam : dd/mm/yy hh/mm/ss\n'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+      child: const Text('Detail Booking'),
+    ),
+            
+              
+                
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 }
